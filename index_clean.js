4dd89f8c1106c257a8a93db14e8ab70194eb1c38
sailesh97 <saileshpatra997@gmail.com>
2022-10-15 23:54:27 +0530

    Cleaning up

---
+++ index_clean.js
@@ -0,0 +1,39 @@
+const cards = document.querySelectorAll(".card");
+
+const observer = new IntersectionObserver(entries => {
+    console.log(entries);
+
+    entries.forEach(entry => {
+        entry.target.classList.toggle("show", entry.isIntersecting)
+        if (entry.isIntersecting) observer.unobserve(entry.target);
+    })
+}, {
+    threshold: 1
+});
+
+cards.forEach(card => {
+    observer.observe(card);
+});
+
+const lastCardObserver = new IntersectionObserver(entries => {
+    const lastCard = entries[0];
+    if (!lastCard.isIntersecting) return;
+    loadNewCards();
+    lastCardObserver.unobserve(lastCard.target);
+    lastCardObserver.observe(document.querySelector(".card:last-child"));
+}, {
+    rootMargin: '100px'
+})
+
+lastCardObserver.observe(document.querySelector(".card:last-child"));
+
+const cardContainer = document.querySelector('.card-container');
+function loadNewCards() {
+    for (let i = 0; i < 10; i++) {
+        const card = document.createElement("div");
+        card.textContent = "New Card";
+        card.classList.add("card");
+        observer.observe(card);
+        cardContainer.append(card);
+    }
+}
